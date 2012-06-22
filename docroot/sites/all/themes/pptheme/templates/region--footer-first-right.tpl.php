fd57c0d2be05c29d7db50a374f4dc3cb9d866e1c
root@piratenpartij.nl <root@piratenpartij.nl>
2012-06-22 16:21:18 +0200

    privacy verklaring + creative commons links in de footer

--- docroot/sites/all/themes/pptheme/templates/region--footer-first-right.tpl.php
+++ docroot/sites/all/themes/pptheme/templates/region--footer-first-right.tpl.php
@@ -4,8 +4,8 @@
   <div>
 	<ul class="ppnl-info-links">
 <li><a href="/contact">Contact</a></li>
-<li><a href="/lalala-cookie?">Blabla Datadingen cookiestuff</a></li>
-<li><a href="/creativecommon">CC-BY-SA 3.0</a></li>
+<li><a href="/privacy-verklaring">Privacyverklaring</a></li>
+<li><a href="https://creativecommons.org/licenses/by-sa/3.0/deed.nl">CC-BY-SA 3.0</a></li>
 	</ul>
   </div>
   <div style="clear: both;">&nbsp;</div>
